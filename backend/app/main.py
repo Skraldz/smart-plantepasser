6b43e6a692ce537463e2fa2a67bd40a4cf210db9
Skraldz <mathaeo@gmail.com>
2026-05-11 14:27:41 +0200

    Added requirements.txt and root health function in main.py

---
+++ backend/app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware              # Allows cross-origin source sharing - Browsers block requests between different origins by default
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[                                              # Allows requests to come from these IP
+        "http://localhost:5173",                                 # For local requests during frontend development
+        "https://plantcloud.mandingo.dk",                        # For requests when in production
+    ],
+    allow_methods=["*"],                                         # Allows all HTTP methods
+    allow_headers=["*"],                                         # Allows all headers
+)
+
+@app.get("/")                                                    # Function to show if the backend is connectable
+def root():
+        return {"status": "ok"}

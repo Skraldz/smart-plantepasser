88efe0dfb9db38cf0181b5c4fdccdc3faa5a519d
Skraldz <mathaeo@gmail.com>
2026-05-12 22:33:56 +0200

    finished save_measurements in crud.py

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -1,8 +1,8 @@
 # Library Imports
 from sqlalchemy.orm import Session
 from datetime import datetime, timedelta
-import models
-import schemas
+from app.models import User, Measurement, SoilReading, WateringEvent
+from app.schemas import MeasurementIn
 
 # This function searches the User table for a user with the typed e-mail
 # It either returns the user with a matching e-mail or None if no match was found
@@ -21,29 +21,37 @@ def create_user(db: Session, email: str, hashed_password: str):
     return new_user
 
 
-# Work in Progress, do not use yet
+# This function takes MeasurementIn from Schemas.py (the pydantic schemas that act with the API) and uses it as data
+# This data then gets added to the database as a new measurement
+# It also loops over the SoilReading and WateringEvent tables, entering data for all plants in both tables
 def save_measurement(db: Session, data: MeasurementIn):
     new_measurement = Measurement(
-        sensor_module_id = sensor_module_id,
-        timestamp = timestamp,
-        temperature = temperature,
-        humidity = humidity,
-        lux = lux,
-        lamp_on = lamp_on
+        sensor_module_id = data.sensor_module_id,
+        timestamp = data.timestamp,
+        temperature = data.temperature,
+        humidity = data.humidity,
+        lux = data.lux,
+        lamp_on = data.lamp_on
     )
+    
+    db.add(new_measurement)
+    db.flush()                      # The database is flushed so measurements gets and ID without being committed
 
-    new_soil_reading = SoilReading(
-        measurement_id = measurement_id,
-        plant_id = plant_id,
-        soil_moisture = soil_moisture
-    )
+    for plant in data.plants:
+        new_soil_reading = SoilReading(
+            measurement_id = new_measurement.id,
+            plant_id = plant.plant_id,
+            soil_moisture = plant.soil_moisture
+        )
+        db.add(new_soil_reading)
 
-    new_watering_event = WateringEvent(
-        plant_id = plant_id,
-        timestamp = timestamp,
-        duration_sec = duration_sec
-    )
-    db.add(new_measurement, new_soil_reading, new_watering_event)
+    for event in data.watering_events:    
+        new_watering_event = WateringEvent(
+            plant_id = plant.plant_id,
+            duration_sec = plant.duration_sec
+        )
+        db.add(new_watering_event)
+    
     db.commit()
-    db.refresh(new_measurement, new_soil_reading, new_watering_event)
-    return new_measurement, new_soil_reading, new_watering_event
+    db.refresh(new_measurement)
+    return new_measurement

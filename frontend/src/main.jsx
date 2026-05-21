2d97d487812af612ed840767578ddbc982ec1cff
PotatoReds <JonathanSJ@outlook.dk>
2026-05-21 11:12:38 +0200

    Fix comments rendering on login page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,17 @@ import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter for ha
 import App from './App'; // Import the main App component which serves as the root of the application
 import './index.css'; // Import the main CSS file for styling the application, which includes Tailwind CSS styles
 
-ReactDOM.createRoot(document.getElementById('root')).render( // Render the application to the DOM
-  <React.StrictMode> // Enable strict mode for highlighting potential problems in the application
-    <BrowserRouter> // Wrap the App component with BrowserRouter to enable routing
-      <App /> // Render the main App component
-    </BrowserRouter> // Close the BrowserRouter component
-  </React.StrictMode> // Enable strict mode for highlighting potential problems in the application
+/* 
+Code telling React to render app into page
+ReactDOM: Render the application to the DOM
+React.StrictMode: Enable strict mode for highlighting potential problems in the application
+BrowserRouter: Wrap the App component with BrowserRouter to enable routing
+App: Render the main App component
+*/ 
+ReactDOM.createRoot(document.getElementById('root')).render( 
+  <React.StrictMode>
+    <BrowserRouter>
+      <App /> 
+    </BrowserRouter>
+  </React.StrictMode>
 );

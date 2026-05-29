82e214846b4ae74cded7f5e5143acca51bbf34ae
Jazkuu <nicholai4450@gmail.com>
2026-05-29 13:37:44 +0200

    Update hub config and RF protocol

--- hub/config.h
+++ hub/config.h
@@ -1,14 +1,14 @@
 #pragma once
 
-#define WIFI_SSID "Airties_Air4960R_3D8F"
-#define WIFI_PASSWORD "kyhckm4388"
+#define WIFI_SSID "WIFI_NAME"
+#define WIFI_PASSWORD "WIFI_PASSWORD"
 
 #define BACKEND_HEALTH_URL "https://plantapi.mandingo.dk/"
 #define BACKEND_URL "https://plantapi.mandingo.dk/api/v1/measurements"
 #define COMMANDS_URL "https://plantapi.mandingo.dk/api/v1/commands/pending"
 #define SETTINGS_URL "https://plantapi.mandingo.dk/api/v1/settings?sensor_module_id=1"
 
-#define DEVICE_SECRET "spis_mere_ost"
+#define DEVICE_SECRET "DEVICE_SECRET"
 
 #define SD_CS_PIN 15
 
